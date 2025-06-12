f706187162a9c96c489c39a5a6e84d096fcb4cb5
bennyhartnett <80306702+bennyhartnett@users.noreply.github.com>
2025-06-11 21:45:39 -0400

    Add schedule grid dialog

--- gui/schedule_dialog.py
+++ gui/schedule_dialog.py
@@ -1,25 +1,44 @@
-"""Dialog for setting a video's scheduled time."""
-from datetime import datetime
+"""Dialog for editing the daily posting schedule template."""
+from __future__ import annotations
+
+from datetime import time
+from typing import Iterable, List
 
 from PySide6 import QtCore, QtWidgets
 
 
 class ScheduleDialog(QtWidgets.QDialog):
-    """Simple datetime picker dialog."""
+    """Table-based editor for daily posting times."""
 
-    def __init__(self, scheduled_at: datetime | None, parent=None):
+    def __init__(self, template: Iterable[time] | None = None, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Schedule Video")
+        self.setWindowTitle("Schedule Template")
 
         layout = QtWidgets.QVBoxLayout(self)
 
-        self.dt = QtWidgets.QDateTimeEdit(self)
-        self.dt.setCalendarPopup(True)
-        if scheduled_at:
-            self.dt.setDateTime(QtCore.QDateTime.fromPython(scheduled_at))
-        else:
-            self.dt.setDateTime(QtCore.QDateTime.currentDateTime())
-        layout.addWidget(self.dt)
+        self.table = QtWidgets.QTableWidget(25, 2, self)
+        self.table.setHorizontalHeaderLabels(["Active", "Time"])
+        self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        self.table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+
+        for row in range(25):
+            chk = QtWidgets.QCheckBox()
+            self.table.setCellWidget(row, 0, chk)
+            t_edit = QtWidgets.QTimeEdit(QtCore.QTime(8, 0))
+            t_edit.setDisplayFormat("HH:mm")
+            self.table.setCellWidget(row, 1, t_edit)
+
+        if template:
+            for row, t in enumerate(template):
+                if row >= 25:
+                    break
+                chk = self.table.cellWidget(row, 0)
+                t_edit = self.table.cellWidget(row, 1)
+                chk.setChecked(True)
+                t_edit.setTime(QtCore.QTime(t.hour, t.minute))
+
+        layout.addWidget(self.table)
 
         buttons = QtWidgets.QDialogButtonBox(
             QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel,
@@ -29,6 +48,16 @@ class ScheduleDialog(QtWidgets.QDialog):
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
 
+    # ------------------------------------------------------------------
     @property
-    def scheduled_at(self) -> datetime:
-        return self.dt.dateTime().toPython()
+    def schedule_template(self) -> List[time]:
+        """Return checked times sorted ascending."""
+        times: List[time] = []
+        for row in range(self.table.rowCount()):
+            chk = self.table.cellWidget(row, 0)
+            t_edit = self.table.cellWidget(row, 1)
+            if isinstance(chk, QtWidgets.QCheckBox) and chk.isChecked():
+                qtime = t_edit.time() if isinstance(t_edit, QtWidgets.QTimeEdit) else QtCore.QTime(0, 0)
+                times.append(qtime.toPython())
+        times.sort()
+        return times
